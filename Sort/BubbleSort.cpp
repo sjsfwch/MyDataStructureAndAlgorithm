7dbfb227b3222a9374b0d386f45f7353d489f1a2
sjsfwch <sjsfwch@vip.qq.com>
2018-09-05 20:02:11 +0800

    Sort

--- Sort/BubbleSort.cpp
+++ Sort/BubbleSort.cpp
@@ -1,30 +1,36 @@
-#include <vector>
 #include <functional>
 #include <iostream>
+#include <vector>
+
+#define SWAP(a, b) \
+    {              \
+        a = a ^ b; \
+        b = a ^ b; \
+        a = a ^ b; \
+    }
 using namespace std;
 
+
 //版本一
-void bubbleSort(vector<int> &A,function<bool(int,int)> const& call){
+void bubbleSort(vector<int>& A, function<bool(int, int)> const& call) {
     bool sorted = false;
-    while(!sorted){
+    while (!sorted) {
         sorted = true;
-        for (int i = 0; i < A.size() - 1;++i){
-            if(!call(A[i],A[i+1])){
-                int temp = A[i];
-                A[i] = A[i + 1];
-                A[i + 1] = temp;
+        for (int i = 0; i < A.size() - 1; ++i) {
+            if (!call(A[i], A[i + 1])) {
+                SWAP(A[i], A[i + 1]);
                 sorted = false;
             }
         }
     }
-
 }
 
-int main(void){
-    vector<int> nums = {1, 2, 4, 8, 6, 9, 15, 16, 18, 38};
+int main(void) {
+    vector<int> nums = {10, 2, 44, 8, 26, 9, 115, 16, 18, 38};
     auto fun = [](int a, int b) { return a < b; };
-    bubbleSort(nums,fun);
-    for(auto i:nums)
+    bubbleSort(nums, fun);
+    for (auto i : nums)
         cout << i << "   ";
+    getchar();
     return 0;
 }
--- Sort/InsertSort.cpp
+++ Sort/InsertSort.cpp
@@ -1,9 +1,32 @@
-#include <vector>
 #include <functional>
 #include <iostream>
+#include <vector>
 using namespace std;
 
+#define SWAP(a, b) \
+    {              \
+        a = a ^ b; \
+        b = a ^ b; \
+        a = a ^ b; \
+    }
+
 //版本一
-void insertSort(vector<int> &A,function<bool(int,int)> const& call){
-    
+void insertSort(vector<int>& A, function<bool(int, int)> const& call) {
+    for (int i = 0; i < A.size() - 1; ++i) {
+        int j = i + 1;
+        while (j > 0 && call(A[j - 1], A[j])) {
+            SWAP(A[j - 1], A[j]);
+            --j;
+        }
+    }
+}
+
+int main(void) {
+    vector<int> nums = {10, 2, 44, 8, 26, 9, 115, 16, 18, 38};
+    auto fun = [](int a, int b) { return a > b; };
+    insertSort(nums, fun);
+    for (auto i : nums)
+        cout << i << "   ";
+    getchar();
+    return 0;
 }
